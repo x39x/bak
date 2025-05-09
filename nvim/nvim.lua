e2c8791402450988de3dad2af5c55502ab168257
Matsu <matsu3x9@icloud.com>
2025-05-09 14:59:48 +0800

    Fri May  9 14:59:48 CST 2025

--- nvim/nvim.lua
+++ nvim/nvim.lua
@@ -33,7 +33,7 @@ local vimtex = {
                 vim.g.vimtex_complete_enabled = 0
                 -- Disable imaps (using Ultisnips)
                 vim.g.vimtex_imaps_enabled = 0
-                -- auto open quickfix on compile erros
+                -- auto open quickfix on compile errors
                 vim.g.vimtex_quickfix_mode = 0
                 vim.g.vimtex_view_method = "skim"
 

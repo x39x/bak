759ede0ccfe69b5e0a8269e98ef894478c8cd1de
Matsu <matsu3x9@icloud.com>
2025-12-28 03:48:31 +0800

    backup

--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -64,3 +64,33 @@ M.rename = function()
                 vim.lsp.buf.rename(new_name)
         end, { buffer = true })
 end
+
+-- pyright
+local lsp_keymaps = require("lspconfig.keymap")
+local python_path = require("utils.python_path")
+pyright = {
+        on_init = function(client)
+                client.config.settings.python.pythonPath = python_path
+        end,
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = {
+                "pyproject.toml",
+                "setup.py",
+                "setup.cfg",
+                "requirements.txt",
+                "Pipfile",
+                "pyrightconfig.json",
+                ".git",
+        },
+        settings = {
+                python = {
+                        analysis = {
+                                typeCheckingMode = "off",
+                        },
+                },
+        },
+        on_attach = function(_, bufnr)
+                lsp_keymaps(bufnr)
+        end,
+}
